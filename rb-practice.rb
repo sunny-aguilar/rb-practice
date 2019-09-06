3af71fd363c3a52e008372548536f6f553f04e22
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-05 17:37:38 -0700

    rb

--- rb-practice.rb
+++ rb-practice.rb
@@ -3,10 +3,10 @@ class Person
   def initialize(name, age)
     puts 'Person initialized'
     @name = name
-    @age = 37
+    @age = age
   end
 
-  
+
 end
 
 p1 = Person.new('Sandro Aguilar', 37)
