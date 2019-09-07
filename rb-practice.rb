f0677ab6f441fe1b5310787825ccffea8fc06b21
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-06 23:11:35 -0700

    rb

--- rb-practice.rb
+++ rb-practice.rb
@@ -143,7 +143,28 @@ list = [1, 2, 3, 4]
 # b = list.collect {|x| 10 * x }
 # puts b
 
-Thread.new do
-  
+def func1
+  i = 0
+  while i<=2
+    puts "func1 at: #{Time.now}"
+    sleep(2)
+    i = i+1
+  end
 end
+
+def func2
+  j = 0
+  while j<=2
+    puts "func2 at: #{Time.now}"
+    sleep(1)
+    j = j+1
+  end
+end
+
+puts "Started At #{Time.now}"
+t1 = Thread.new{func1()}
+t2 = Thread.new{func2()}
+t1.join
+t2.join
+puts "End at #{Time.now}"
 
